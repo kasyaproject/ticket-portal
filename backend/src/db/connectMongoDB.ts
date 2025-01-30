a9f0efd6a4746b08cde975f17b728a63777eb6dd
sikadep <andikayamana14045@gmail.com>
2025-01-30 20:22:15 +0700

    refactor: enhance MongoDB connection handling and restructure server initialization

--- backend/src/db/connectMongoDB.ts
+++ backend/src/db/connectMongoDB.ts
@@ -1,7 +1,12 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const connectMongoDB = async () => {
   const mongoUri = process.env.MONGO_URI || ""; // ambil MONGO_URI dari .env
+  if (!mongoUri)
+    throw new Error("MONGO_URI is not defined in environment variables");
 
   try {
     await mongoose.connect(mongoUri);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,24 +6,34 @@ import bodyParser from "body-parser";
 import authRouter from "./routes/auth.routes";
 import connectMongoDB from "./db/connectMongoDB";
 
-const app = express(); // Untuk membuat aplikasi express
+async function init() {
+  try {
+    // Koneksi ke mongoDb
+    const db = await connectMongoDB();
 
-dotenv.config(); // untuk mengambil nilai dari file .env
-app.use(bodyParser.json()); // Untuk membaca json dari req.body
-app.use(express.urlencoded({ extended: true })); // (Opsional) Untuk form-data
-const PORT = process.env.PORT; // Ambil PORT dari .env
+    console.log("Database Status: ", db);
 
-// Route API
-app.get("/", (req, res) => {
-  res.status(200).json({
-    message: "Welcome to API for App Ticket Portal by Andika Syamsiana",
-  });
-});
-app.use("/api/auth", authRouter);
+    const app = express(); // Untuk membuat aplikasi express
 
-app.listen(PORT, async () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
+    dotenv.config(); // untuk mengambil nilai dari file .env
+    app.use(bodyParser.json()); // Untuk membaca json dari req.body
+    app.use(express.urlencoded({ extended: true })); // (Opsional) Untuk form-data
+    const PORT = process.env.PORT; // Ambil PORT dari .env
 
-  const result = await connectMongoDB();
-  console.log(result);
-});
+    // Route API
+    app.get("/", (req, res) => {
+      res.status(200).json({
+        message: "Welcome to API for App Ticket Portal by Andika Syamsiana",
+      });
+    });
+    app.use("/api/auth", authRouter);
+
+    app.listen(PORT, async () => {
+      console.log(`Server is running on port http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+init();
