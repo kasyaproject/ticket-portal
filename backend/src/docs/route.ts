bd32e3f4c3b8fea936d4bb507064ea4d5268840b
sikadep <andikayamana14045@gmail.com>
2025-02-01 19:10:07 +0700

    fix: downgrade swagger-ui-express to version 4.6.3 and update related dependencies

--- backend/src/docs/route.ts
+++ backend/src/docs/route.ts
@@ -1,17 +1,10 @@
 import { Express } from "express";
-import express from "express";
 import swaggerUi from "swagger-ui-express";
 import swaggerOutput from "./swagger_output.json";
 import fs from "fs";
 import path from "path";
 
 export default function docs(app: Express) {
-  // Serve static files
-  app.use(
-    "/swagger-ui-dist",
-    express.static(path.join(__dirname, "../public/swagger-ui-dist"))
-  );
-
   const css = fs.readFileSync(
     path.resolve(
       __dirname,
